21761303d117464e043fb47d8f686faf42c2b48a
Bach <Bach@MBB.local>
2017-10-09 20:58:40 +0200

    typo

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  recast: process.env.ERECAST_KEY,
+  recast: process.env.RECAST_KEY,
   appId: process.env.APP_ID,
   appPassword: process.env.APP_ID,
   flickrKey: process.env.FLICKR_KEY,
