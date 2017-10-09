97c36f6c7758be14bb9ab6b7e7f741413be023ac
Bach <Bach@MBB.local>
2017-10-09 21:00:52 +0200

    typo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   recast: process.env.RECAST_KEY,
   appId: process.env.APP_ID,
-  appPassword: process.env.APP_ID,
+  appPassword: process.env.APP_PWD,
   flickrKey: process.env.FLICKR_KEY,
 }
 module.exports = config
