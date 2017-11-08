c8fc551b6fa52e112128000e9e857828c6ef1586
Bach <Bach@MBB.local>
2017-11-08 21:30:08 +0100

    add fb

--- pokebot.js
+++ pokebot.js
@@ -111,3 +111,81 @@ console.log("listning to port " + portnumber)
 server.post('/', connector.listen())
 //db.insertUser(obj2);
 //db.getUsers();
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Bonjour, Je suis votre Facebook Messenger Bot')
+})
+
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'Bot_Messenger_App') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+
+// End Point
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            if (text === 'salut') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "Bot: " + text.substring(0, 200))
+        }
+        if (event.postback) {
+            text = JSON.stringify(event.postback)
+            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+            continue
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "EAAU6bUxOrIYBAGt9uqgYGX2HwlxycaD3Uhv7M3gSGUxalu2MsqSDZAhruN5Ve6gXJdu7zhPv4Ry3sU4mRCc5pHIi8bSRUbaVvJVEL0qNHNyVyFZBYGpcCZBdCgHPeWILbe6RFwBuTh80ytfmpXQp2g4QVhFFQ54lbIdDZAZBCngZDZD"
+
+// Echo back messages
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
